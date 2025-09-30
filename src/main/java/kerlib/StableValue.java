8b8d35fd99fcd23bc91453153ce6881e3e8b9df8
Ilia Pushkin <85931237+Kerravitarr@users.noreply.github.com>
2025-09-30 17:21:47 +0300

    Копия StableValue
    ПОка он находится в превьюшке у java, а мне он уже нужен. Сейчас сделаю затычку, а потом заменю на правильную версию :)

---
+++ src/main/java/kerlib/StableValue.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package kerlib;
+
+/**
+ *Пока он находится в превью, сделал для себя временную копию
+ * @author Kerravitarr (github.com/Kerravitarr)
+ */
+public class StableValue<T> {
+    ///Значение уже сохранялось?
+    private boolean isSet = false;
+    
+    public static <T> StableValue<T> supplier(java.util.function.Supplier<T> getter){
+        
+    }
+    public T get(){
+        
+    }
+}
